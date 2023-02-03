4a4aacd297afcb2fe8a50d8ac6c87f688847fc25
Daniel Luz <danielfmluz@gmail.com>
2023-02-03 15:17:01 -0300

    Aula Operadores

---
+++ Programação extrema/AulaExemplo04Exercício.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AulaExemplo04Exercício {
+    public static void main(String[] args) {
+        
+        double nota1 = 9.0;
+        double nota2 = 10;
+
+        double media;
+
+        media = (nota1 + nota2)/2;
+
+        System.out.println(media);
+
+        //----------------------------------------------------------------------------------
+
+        int valor = 15;
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o valor:");
+        valor = teclado.nextInt();
+
+        System.out.println(valor/2);
+        System.out.println(valor%2);
+
+    }
+}
